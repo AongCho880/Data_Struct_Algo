3fa0692b0596200c726478475fa7374b66a99f91
AongCho880 <aongcho880@gmail.com>
2023-02-18 21:23:14 +0600

    Update Bubble_Sort

---
+++ C_lang/Sorting_Algo/Bubble_Sort/bubble_sort.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+// ......... Get input from bubble_sort.txt file ...........
+void read_file(int *arr, int *size){
+    FILE *fptr = fopen("bubble_sort.txt","r");
+    if(fptr==NULL){
+        printf("File doesn't exist !\n");
+    }
+    else{
+        fscanf(fptr,"%d",size);
+        for(int i=0;i<*size;i++){
+            fscanf(fptr,"%d",&arr[i]);
+        }
+        fclose(fptr);
+    }
+}
+
+// ............. Swap function ..............
+void swap(int *a, int *b){
+    int temp=*a;
+    *a = *b;
+    *b = temp;
+}
+
+// ................ Bubble Sort .............
+void bubble_sort(int *arr, int size){
+    for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-i-1;j++){
+            if(arr[j]>arr[j+1])
+                swap(&arr[j],&arr[j+1]);
+        }
+    }
+}
+
+// ........... Main Function ............
+int main(){
+    int size;
+    int *arr;
+    read_file(arr,&size);
+    bubble_sort(arr,size);
+    for(int i=0;i<size;i++)
+        printf("%d ",arr[i]);
+    return 0;
+}
