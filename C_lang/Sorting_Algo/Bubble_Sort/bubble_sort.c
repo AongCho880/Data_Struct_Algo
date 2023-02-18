73ec85055443ad77790962e5a7a94818a74a9923
AongCho880 <aongcho880@gmail.com>
2023-02-18 22:05:04 +0600

    Memory allocate for arr in bubble_sort.c

--- C_lang/Sorting_Algo/Bubble_Sort/bubble_sort.c
+++ C_lang/Sorting_Algo/Bubble_Sort/bubble_sort.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 // ......... Get input from bubble_sort.txt file ...........
 void read_file(int *arr, int *size){
@@ -35,10 +36,12 @@ void bubble_sort(int *arr, int size){
 // ........... Main Function ............
 int main(){
     int size;
-    int *arr;
+    int *arr = (int *)malloc(100*sizeof(int)); // Memory allocation for the arr
     read_file(arr,&size);
     bubble_sort(arr,size);
     for(int i=0;i<size;i++)
         printf("%d ",arr[i]);
+    
+    free(arr); // Memory deallocation
     return 0;
 }
