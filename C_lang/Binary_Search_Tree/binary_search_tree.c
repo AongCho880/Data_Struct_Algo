e7445ed0fc25391bbcb96a43758509d10c8c15a8
AongCho880 <aongcho880@gmail.com>
2023-02-25 00:33:42 +0600

    Binary Search Tree Implementation with C

---
+++ C_lang/Binary_Search_Tree/binary_search_tree.c
@@ -0,0 +1,33 @@
+// ........ Binary Search Tree ............
+// ........... Main Function ..............
+
+#include<stdio.h>
+#include "bst.h"
+
+int main(){
+    Node *root = NULL;
+    int n;
+    printf("Numebr of node: ");
+    scanf("%d",&n);
+
+    // Data insertion ..............
+    printf("\nInsert data: ");
+    for(int i=0;i<n;i++){
+        int data; scanf("%d",&data);
+        root = insert_in_bst(root,data);
+    }
+
+    // Pre_Order Sequence ...........
+    printf("\nPre_order sequence: ");
+    pre_order(root);
+
+    // In_Order Sequence ............
+    printf("\nIn_order sequence: ");
+    in_order(root);
+
+    // Post_Order Sequence ...........
+    printf("\nPost_order sequence: ");
+    post_order(root);
+
+    return 0;
+}
---
+++ C_lang/Binary_Search_Tree/bst.h
@@ -0,0 +1,72 @@
+// ........... Binary Search Tree Implementation ............
+// ........... Author: Aong Cho Marma .......................
+// .................. Header File ...........................
+
+#ifndef bst
+#define bst
+
+#include<stdio.h>
+#include<stdlib.h>
+
+// Define structure .............
+typedef struct Node{
+    int data;
+    struct Node *left;
+    struct Node *right;
+}Node;
+
+// Create a new node ............
+Node *create_node(int val){
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = val;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
+// Insert node in Binary Search Tree ..........
+Node *insert_in_bst(Node *root,int val){
+    if(root==NULL){
+        return create_node(val);
+    }
+    else if(root->data > val){
+        root->left = insert_in_bst(root->left,val);
+    }
+    else{
+        root->right = insert_in_bst(root->right,val);
+    }
+    return root;
+}
+
+// Pre_Order Traversal ................
+void pre_order(Node *root){
+    if(root == NULL){
+        return;
+    }
+    printf("%d ",root->data);
+    pre_order(root->left);
+    pre_order(root->right);
+}
+
+// In_Order Traversal .............
+void in_order(Node *root){
+    if(root==NULL){
+        return;
+    }
+    in_order(root->left);
+    printf("%d ",root->data);
+    in_order(root->right);
+}
+
+// Post_Order Traversal .........
+void post_order(Node *root){
+    if(root==NULL){
+        return;
+    }
+    post_order(root->left);
+    post_order(root->right);
+    printf("%d ",root->data);
+}
+
+#endif
