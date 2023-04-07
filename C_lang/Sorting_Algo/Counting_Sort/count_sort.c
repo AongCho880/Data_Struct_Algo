51b2ba067fcc86f91b2a4898195066eeb402b6e5
AongCho880 <aongcho880@gmail.com>
2023-04-07 16:57:21 +0600

    Counting Sort with C

--- C_lang/Sorting_Algo/Counting_Sort/count_sort.c
+++ C_lang/Sorting_Algo/Counting_Sort/count_sort.c
@@ -1,74 +1,96 @@
+// ................ Counting Sort with C ............
+
 #include<stdio.h>
 #include<stdlib.h>
 
-
-// Read data from count_sort.txt file ..........
-int *read_file(int *size, int *mx){
+// Get data from "count_sort.txt" file ......
+int *read_file(int *size){
     FILE *fptr = fopen("count_sort.txt","r");
     if(fptr==NULL){
         return NULL;
     }
-    else{
-        fscanf(fptr,"%d",size);
-        int *arr = calloc(*size,sizeof(int)); // allocate memory for arr
-        for(int i=0;i<*size;i++){
-            fscanf(fptr,"%d",&arr[i]);
-            if(arr[i]>*mx){
-                *mx = arr[i];   // update max data if arr[i] is greater
-            } 
-        }
-        fclose(fptr);
 
-        return arr;
+    fscanf(fptr,"%d",size);
+
+    int *arr = (int *)calloc(*size,sizeof(int)); // memory allocation
+    for(int i=0;i<*size;i++){
+        fscanf(fptr,"%d",&arr[i]);
     }
+    fclose(fptr);
+    return arr;
 }
 
-// .............. Counting Sort ...................
-int *count_sort(int *arr, int size, int mx){
-    int *count = calloc(mx+1,sizeof(int));
-    
+// Counting Sort ............
+void counting_sort(int *arr, int size){
+    int max = arr[0];
+
+    // Find the max element
+    for(int i=0;i<size;i++){
+        if(max<arr[i]){
+            max = arr[i];
+        }
+    }
+
+    // Memory allocation
+    int *count = (int *)calloc(max+1,sizeof(int));
+    int *output = (int *)calloc(size,sizeof(int));
+
+    // Count the number of occurance of each element
     for(int i=0;i<size;i++){
         count[arr[i]]++;
     }
 
-    for(int i=1;i<mx+1;i++){
+    // Compute the prefix sum
+    for(int i=1;i<=max;i++){
         count[i] += count[i-1];
     }
 
-    int *output = calloc(size,sizeof(int));
-    for(int i=size-1;i>=0;i--){  // to ensure stability, traverse in reverse order
+    // Place the elements in the sorted order
+    for(int i=size-1;i>=0;i--){
         output[count[arr[i]]-1] = arr[i];
         count[arr[i]]--;
     }
+
+    // Copy the sorted elements to the original array
+    for(int i=0;i<size;i++){
+        arr[i] = output[i];
+    }
+
     free(count);
-    return output;
+    free(output);
 }
 
-
-// ............ Main Function .............
+// Main Function .........
 int main(){
-    int size=0,mx=0;
-    int *arr = read_file(&size,&mx);
+    int size=0;
+    int *arr = read_file(&size);
 
     if(arr==NULL){
-        printf("File doesn't exist!\n");
-        return 1;
-    }
-    else{
-        int *output = count_sort(arr,size,mx);
-
-        printf("Raw data:\n");
-        for(int i=0;i<size;i++){
-            printf("%d ",arr[i]);
-        }
-
-        printf("\nSorted data:\n");
-        for(int i=0;i<size;i++){
-            printf("%d ",output[i]);
-        }
-        
-        free(arr); free(output);
-
+        printf("\nUnable to create array !!!\n");
         return 0;
     }
+
+    printf("Unsorted: ");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+    
+    counting_sort(arr,size);
+
+    printf("\nSorted: ");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+
+    free(arr);
+    return 0;
 }
+
+// ...... Time Complexity .......
+// Find max      : O(n)
+// Count elements: O(n)
+// Prefix sum    : O(k)   [k = range of element]
+// Sort elements : O(n)
+// Copy elements : O(n)
+// ----------------------
+// Complexity    : O(n+k)
--- C_lang/Sorting_Algo/Test_1.c
+++ C_lang/Sorting_Algo/Test_1.c
@@ -1,35 +1,96 @@
-#include <stdio.h>
-#include <stdlib.h>
+// ................ Counting Sort with C ............
 
-int *count_sort(int *arr, int size, int mx){
-    int *count = calloc(mx+1,sizeof(int));
-    
+#include<stdio.h>
+#include<stdlib.h>
+
+// Get data from "count_sort.txt" file ......
+int *read_file(int *size){
+    FILE *fptr = fopen("test.txt","r");
+    if(fptr==NULL){
+        return NULL;
+    }
+
+    fscanf(fptr,"%d",size);
+
+    int *arr = (int *)calloc(*size,sizeof(int)); // memory allocation
+    for(int i=0;i<*size;i++){
+        fscanf(fptr,"%d",&arr[i]);
+    }
+    fclose(fptr);
+    return arr;
+}
+
+// Counting Sort ............
+void counting_sort(int *arr, int size){
+    int max = arr[0];
+
+    // Find the max element
+    for(int i=0;i<size;i++){
+        if(max<arr[i]){
+            max = arr[i];
+        }
+    }
+
+    // Memory allocation
+    int *count = (int *)calloc(max+1,sizeof(int));
+    int *output = (int *)calloc(size,sizeof(int));
+
+    // Count the number of occurance of each element
     for(int i=0;i<size;i++){
         count[arr[i]]++;
     }
 
-    for(int i=1;i<mx+1;i++){
+    // Compute the prefix sum
+    for(int i=1;i<=max;i++){
         count[i] += count[i-1];
     }
 
-    int *output = calloc(size,sizeof(int));
-    for(int i=0;i<size;i++){
+    // Place the elements in the sorted order
+    for(int i=size-1;i>=0;i--){
         output[count[arr[i]]-1] = arr[i];
         count[arr[i]]--;
     }
+
+    // Copy the sorted elements to the original array
+    for(int i=0;i<size;i++){
+        arr[i] = output[i];
+    }
+
     free(count);
-    return output;
+    free(output);
 }
 
+// Main Function .........
+int main(){
+    int size=0;
+    int *arr = read_file(&size);
 
-int main() {
-    int arr[] = {5,5,2,1,3,3,7,1,2,3};
-    int size = 10, mx = 7;
+    if(arr==NULL){
+        printf("\nUnable to create array !!!\n");
+        return 0;
+    }
 
-    int *out = count_sort(arr,size,mx);
+    printf("Unsorted: ");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+    
+    counting_sort(arr,size);
 
-    for(int i=0;i<size;i++)
-        printf("%d ",out[i]);
-    free(out);
+    printf("\nSorted: ");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+
+    free(arr);
     return 0;
 }
+
+// ...... Time Complexity .......
+// Find max      : O(n)
+// Count elements: O(n)
+// Prefix sum    : O(k)   [k = range of element]
+// Sort elements : O(n)
+// Copy elements : O(n)
+// ----------------------
+// Complexity    : O(n+k)
